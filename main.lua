b34cf31b17ea1d02a15e3f87319f4c435d24aaad
kamackey1988 <kimberlyspayde18@yahoo.com>
2022-02-21 22:09:45 -0500

    update for other comp

--- main.lua
+++ main.lua
@@ -1,4 +1,16 @@
 function love.load()
+    anim8 = require 'libraries/anim8/anim8'
+
+    sprites = {}
+    sprites.playerSheet = love.graphics.newImage('sprites/playerSheet.png')
+
+    local grid = anim8.newGrid(math.floor(sprites.playerSheet:getWidth()/15), math.floor(sprites.playerSheet:getHeight()/3), sprites.playerSheet:getWidth(), sprites.playerSheet:getHeight())
+
+    animations = {}
+    animations.idle = anim8.newAnimation(grid('1-15',1), 0.5) -- 1-15 that we want the first 15 pictures then 1 to indicate row 1 then how long you want each animation to run for 
+    animations.jump = anim8.newAnimation(grid('1-7',2), 0.5)
+    animations.run = anim8.newAnimation(grid('1-15',3), 0.5)
+
     wf = require 'libraries/windfield/windfield' -- include the windfield folder for physics 
     world = wf.newWorld(0,800, false)  --create a world for the physics world. paramters are for gracity 0 for up 0 for down if set to 100 would have them going down
     world:setQueryDebugDrawing(true)
@@ -11,6 +23,7 @@ function love.load()
     player = world:newRectangleCollider(360, 100, 80, 80, {collision_class = "player"})
     player:setFixedRotation(true) --so player doesn't rotate while falling
     player.speed = 240
+    player.animation = animations.run
 
     --creates something for the player object to land on still moves need to change the type.
     platform = world:newRectangleCollider(250, 400, 300, 100, {collision_class = "platform"})
@@ -41,10 +54,12 @@ function love.update(dt)
         end
     end
 
+    player.animation:update(dt)
 end
 
 function love.draw()
     world:draw() -- dont want to keep when game is active but good to have while programing for debugging 
+    player.animation:draw(sprites.playerSheet, 0, 0, nil, 0.25)
 
 end 
 
