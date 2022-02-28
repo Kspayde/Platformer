69ab11f271e8b19140528c7c7b32531c7a3210da
kamackey1988 <kimberlyspayde18@yahoo.com>
2022-02-28 10:03:34 -0500

    new file update

--- main.lua
+++ main.lua
@@ -5,7 +5,7 @@ function love.load()
     sprites.playerSheet = love.graphics.newImage('sprites/playerSheet.png')
 
     local grid = anim8.newGrid(614, 564, sprites.playerSheet:getWidth(), sprites.playerSheet:getHeight()) 
-    
+
     animations = {}
     animations.idle = anim8.newAnimation(grid('1-15',1), 0.5) -- 1-15 that we want the first 15 pictures then 1 to indicate row 1 then how long you want each animation to run for 
     animations.jump = anim8.newAnimation(grid('1-7',2), 0.5)
@@ -19,11 +19,11 @@ function love.load()
     world:addCollisionClass('player' --[[ {ignores = {'platform'}} ]])
     world:addCollisionClass('Danger')
 
-    -- creates a collider an object that holds all physsics about something in this case the player
-    player = world:newRectangleCollider(360, 100, 80, 80, {collision_class = "player"})
-    player:setFixedRotation(true) --so player doesn't rotate while falling
-    player.speed = 240
-    player.animation = animations.run
+    -- adding our new playerlua file
+
+    require('player')
+
+
 
     --creates something for the player object to land on still moves need to change the type.
     platform = world:newRectangleCollider(250, 400, 300, 100, {collision_class = "platform"})
@@ -40,34 +40,18 @@ end
 function love.update(dt)
     world:update(dt)
 
-    if player.body then 
-        local px, py = player:getPosition() -- gets player x and y positions and fills it int he px and py
-        if love.keyboard.isDown('right') then 
-            player:setX(px + player.speed*dt) --sets players x pos current position plus 5 so moves right -- changed five to vairable playerspeed
-        end
-        if love.keyboard.isDown('left') then 
-            player:setX(px - player.speed*dt) --sets players x pos current position minus 5 so moves left -- changed five to vairable playerspeed
-        end
-
-        if player:enter('Danger') then
-            player:destroy()
-        end
-    end
-
-    player.animation:update(dt)
 end
 
 function love.draw()
     world:draw() -- dont want to keep when game is active but good to have while programing for debugging 
-    player.animation:draw(sprites.playerSheet, 0, 0, nil, 0.25)
-
+    
+   
 end 
 
 function love.keypressed(key)
     if key == 'up' then 
-        local colliders = world:queryRectangleArea(player:getX() - 40, player:getY() + 40, 80, 2, {'platform'})
-        if #colliders > 0 then 
-            player:applyLinearImpulse(0, -7000)  --make a player jump by pressing upkey
+        if player.grounded then 
+            player:applyLinearImpulse(0, -4000)  --make a player jump by pressing upkey
         end
     end
 end
---
+++ player.lua
@@ -0,0 +1,55 @@
+    -- creates a collider an object that holds all physsics about something in this case the player
+    player = world:newRectangleCollider(360, 100, 40, 100, {collision_class = "player"})
+    player:setFixedRotation(true) --so player doesn't rotate while falling
+    player.speed = 240
+    player.animation = animations.idle
+    player.isMoving = false
+    player.direction = 1
+    player.grounded = true
+
+function playerUpdate(dt)
+    if player.body then 
+        local colliders = world:queryRectangleArea(player:getX() - 20, player:getY() + 50, 40, 2, {'platform'})
+        if #colliders > 0 then 
+            player.grounded = true
+        else
+            player.grounded = false 
+        end
+
+        player.isMoving = false
+        local px, py = player:getPosition() -- gets player x and y positions and fills it int he px and py
+        if love.keyboard.isDown('right') then 
+            player:setX(px + player.speed*dt) --sets players x pos current position plus 5 so moves right -- changed five to vairable playerspeed
+            player.isMoving = true
+            player.direction = 1
+        end
+        if love.keyboard.isDown('left') then 
+            player:setX(px - player.speed*dt) --sets players x pos current position minus 5 so moves left -- changed five to vairable playerspeed
+            player.isMoving = true
+            player.direction = -1
+        end
+
+        if player:enter('Danger') then
+            player:destroy()
+        end
+    end
+    
+    if player.grounded then 
+        if player.isMoving then 
+            player.animation = animations.run
+        else
+            player.animation = animations.idle
+        end
+    else
+        player.animation = animations.jump
+    end
+
+    player.animation:update(dt)
+end
+
+
+function drawPlayer()
+    local px, py = player:getPosition()          -- multipluing .25 by negative will flip image making it face left leaving it pos will keep it right
+    player.animation:draw(sprites.playerSheet, px, py, nil, 0.25 * player.direction, 0.25, 130, 300)
+
+end
