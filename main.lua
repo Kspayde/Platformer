cd81a6cbdd489b9987ffa5da177b4b04830906be
kamackey1988 <kimberlyspayde18@yahoo.com>
2022-02-28 20:58:29 -0500

    finished with cam

--- main.lua
+++ main.lua
@@ -3,7 +3,10 @@ function love.load()
 
     anim8 = require 'libraries/anim8/anim8'
     sti = require 'libraries/Simple-Tiled-Implementation-Master/sti'
-    cameraFile = require 'librtaries/hump/camera'
+    cameraFile = require 'libraries/hump/camera'
+    
+    -- creating cam to make camera file an obj
+    cam = cameraFile()
 
     sprites = {}
     sprites.playerSheet = love.graphics.newImage('sprites/playerSheet.png')
@@ -51,17 +54,25 @@ function love.update(dt)
     gameMap:update(dt)
     --calling update file from player
     playerUpdate(dt)
+    -- note 2 need to get players position 
+    local px, py = player:getPosition()
+    -- note 1  make camera follow player ** makes cam look at a specific point in the game
+    cam:lookAt(px, love.graphics.getHeight()/2) -- cam use (px, py) but screen can get off centered so best to use the the game map for center 
+                                                -- so when play jumps or goes on a lower platform it doesn't move down    
 
 end
 
 function love.draw()
-    --world:draw() -- dont want to keep when game is active but good to have while programing for debugging 
-    -- draw the map 
-    gameMap:drawLayer(gameMap.layers["Tile Layer 1"])
-    world:draw() -- dont want to keep when game is active but good to have while programing for debugging  move after drawlayer so we can see it better 
-    
-    --calling draw from player file
-     drawPlayer()
+    -- have to call camera want to do that first so everything else is drawn over the camera 
+    cam:attach()    
+        --world:draw() -- dont want to keep when game is active but good to have while programing for debugging 
+        -- draw the map 
+        gameMap:drawLayer(gameMap.layers["Tile Layer 1"])
+        world:draw() -- dont want to keep when game is active but good to have while programing for debugging  move after drawlayer so we can see it better 
+        
+        --calling draw from player file
+        drawPlayer()
+    cam:detach() -- everything should be inside the cam attach and detach unless its something you always want on the screen no matter where the cam is looking like a health bar 
    
 end 
 
